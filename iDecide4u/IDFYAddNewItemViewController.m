64aea4e9dd9e9dad625e2d6f7fb3ccb7aa91beb0
Dominic Frei <dominic.frei@mac.com>
2015-03-14 18:43:24 +0100

    IDFY-2: Add 'IDFYAddNewItemViewController' and implement the done button, which leads back to the main scene.

---
+++ iDecide4u/IDFYAddNewItemViewController.m
@@ -0,0 +1,31 @@
+//
+//  AddNewItemViewController.m
+//  iDecide4u
+//
+//  Created by Dominic Frei on 14/03/2015.
+//  Copyright (c) 2015 dominicfrei.com. All rights reserved.
+//
+
+#import "IDFYAddNewItemViewController.h"
+
+@interface IDFYAddNewItemViewController ()
+
+@end
+
+@implementation IDFYAddNewItemViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)doneButtonPressed:(id)sender {
+    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
