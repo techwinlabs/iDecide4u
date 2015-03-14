4fa1c4901823c68f80e44c576d11b79e7f04cf91
Dominic Frei <dominic.frei@mac.com>
2015-03-14 20:18:31 +0100

    IDFY-8: Add IDFYMainSceneViewController, implement the IDFYAddNewItemDelegate protocol method saveNewItem,

---
+++ iDecide4u/IDFYMainSceneViewController.m
@@ -0,0 +1,31 @@
+//
+//  IDFYMainSceneViewController.m
+//  iDecide4u
+//
+//  Created by Dominic Frei on 14/03/2015.
+//  Copyright (c) 2015 dominicfrei.com. All rights reserved.
+//
+
+#import "IDFYMainSceneViewController.h"
+
+@interface IDFYMainSceneViewController ()
+
+@end
+
+@implementation IDFYMainSceneViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)saveNewItem:(NSString *)newItem {
+    
+}
+
+@end
