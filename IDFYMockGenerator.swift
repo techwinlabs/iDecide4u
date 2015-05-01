24e962d57665a1a277d2c9446a6ab84aeb3bb887
Dominic Frei <dominic@dominicfrei.com>
2015-05-01 18:05:49 +0200

    IDFY-18: Add a mock generator (initially creating a meal list with three empty entries).

---
+++ IDFYMockGenerator.swift
@@ -0,0 +1,28 @@
+//
+//  IDFYMockGenerator.swift
+//  iDecide4U
+//
+//  Created by Dominic Frei on 01/05/2015.
+//  Copyright (c) 2015 dominicfrei.com. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+private let managedObjectContext = IDFYCoreDataStack.sharedCoreDataStack().managedObjectContext!
+private let optionListEntityName = IDFYCoreDataStack.sharedCoreDataStack().optionListEntityName
+
+class IDFYMockGenerator {
+  
+  class func generateMockIntoDatabase() {
+    
+    let fetchResult = IDFYCommonDataManager.fetchEntity(optionListEntityName, fromManagedObjectContext: managedObjectContext, withPredicate: NSPredicate(format: "name == 'Meals'")) as! [IDFYOptionList]
+    if 0 == fetchResult.count {
+      let newList = NSEntityDescription.insertNewObjectForEntityForName(IDFYCoreDataStack.sharedCoreDataStack().optionListEntityName, inManagedObjectContext: managedObjectContext) as? IDFYManagedOptionList
+      newList!.name = "Meals"
+      newList!.options = ["", "", ""]
+    }
+    
+  }
+  
+}
